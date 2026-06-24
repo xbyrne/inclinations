7abb02602ce449a2d2dea6e5432e479415e2868f
Xander Byrne <ajnb3@cam.ac.uk>
2026-06-24 12:29:35 +0100

    Tweak Hill stability plot

--- scripts/figs/plot_hillstability.py
+++ scripts/figs/plot_hillstability.py
@@ -81,12 +81,21 @@ def main():
     print("  Plotting...")
 
     fg, axs = plt.subplots(
-        3, 2, figsize=(6, 7), gridspec_kw={"hspace": 0.0, "wspace": 0.0}
+        3, 2, figsize=(5, 6), gridspec_kw={"hspace": 0.0, "wspace": 0.0}
     )
 
     i_deg = np.linspace(0, 90, 1000)
     sini_1_3 = np.sin(np.radians(i_deg)) ** (1 / 3)
-
+    Y_MAX = 24
+    TITLES = [
+        "HD 158259",
+        "HD 215152",
+        "Barnard's Star",
+        "HD 184010",
+        "HD 28471",
+        "YZ Ceti",
+    ]
+    TITLE_DICT = dict(zip(SYSTEMS, TITLES))
     for i, system in enumerate(SYSTEMS):
         ax = axs.flatten()[i]
         system_data = planets_data[planets_data["hostname"] == system]
@@ -97,27 +106,41 @@ def main():
         p_day = system_data["pl_orbper"].values
         a_au = (p_day / 365.25) ** (2 / 3) * (m_star / M_SUN) ** (1 / 3)
 
-        for (a1, a2), (m1, m2) in zip(
-            zip(a_au[:-1], a_au[1:]), zip(m_min[:-1], m_min[1:])
+        for n, ((a1, a2), (m1, m2)) in enumerate(
+            zip(zip(a_au[:-1], a_au[1:]), zip(m_min[:-1], m_min[1:]))
         ):
+            if n == 0:
+                delta_min = np.inf
+                closest_pair = (0, 1)
             Delta = Delta_factor(a1, a2, m1, m2, m_star)
-            ax.plot(i_deg, Delta * sini_1_3, color="k", lw=1.5)
+            if Delta < delta_min:
+                delta_min = Delta
+                closest_pair = (n, n + 1)
+
+        closest_planets = "".join(
+            [system_data.iloc[p].pl_name[-1] for p in list(closest_pair)]
+        )
+        title = TITLE_DICT[system] + "\n" + f"$\\Delta_{{\\rm {closest_planets}}}$"
+
+        ax.plot(i_deg, delta_min * sini_1_3, color="k", lw=1.5)
 
         ax.set_xlim(0, 90)
         ax.set_xticks([0, 30, 60, 90])
         ax.set_xticklabels([])
-        ax.set_ylim(0, 27)
+        ax.set_ylim(0, Y_MAX)
         ax.set_yticks([0, 10, 20])
 
-        # ax.axhline(10, color="k", ls="--", lw=1.5)
-        ax.fill_between(i_deg, 0, 10, color="r", alpha=0.3)
+        ax.fill_between(i_deg, 0, 10, color="r", alpha=0.2)
 
         inclination_limit = inclinations_5pc[system]
-        ax.vlines(inclination_limit, 0, 27, color="k", lw=1.5, alpha=0.5)
-        ax.vlines(PRIOR_5PC, 0, 27, color="k", ls="--", lw=1.5, alpha=0.5)
+        ax.vlines(
+            PRIOR_5PC, 8, 12, color="k", ls=":", lw=1.5, alpha=0.5, label="5% prior"
+        )
+        ax.vlines(
+            inclination_limit, 8, 12, color="k", lw=1.5, alpha=0.5, label="5% post."
+        )
 
-        title = system if system != "Barnard's star" else "Barnard"
-        ax.set_title(title, fontsize=15, loc="right", x=0.97, y=0.78)
+        ax.set_title(title, fontsize=15, ha="left", va="top", x=0.06, y=0.87)
 
     axs[-1, 0].set_xticks([0, 30, 60, 90], labels=["0", "30", "60", ""], fontsize=14)
     axs[-1, 1].set_xticks([0, 30, 60, 90], labels=["0", "30", "60", "90"], fontsize=14)
@@ -126,27 +149,32 @@ def main():
 
     for ax in axs.flatten():
         ax.tick_params(
-            length=5, labelsize=13, width=1.5, direction="in", top=True, right=True
+            length=3, labelsize=13, width=1.5, direction="in", top=True, right=True
         )
         for spine in ax.spines.values():
             spine.set_linewidth(1.5)
 
     # Label CINEMAS limits
-    ax = axs[0, 0]
-    ax.annotate(
-        "Posterior", xy=(27, 19.5), va="center", fontsize=12, rotation=90, alpha=0.7
+    axs[-1, -1].legend(
+        loc=[0.47, 0.5],
+        fontsize=10,
+        frameon=False,
+        handlelength=1.5,
+        handletextpad=0.5,
     )
-    ax.annotate(
-        "Prior", xy=(11, 19.5), va="center", fontsize=12, rotation=90, alpha=0.7
+
+    # Label Chambers+96 limit
+    axs[0, 0].annotate(
+        "$\\Delta < 10$",
+        (55, 4.8),
+        fontsize=14,
+        va="center",
+        ha="center",
+        color="r",
+        alpha=0.7,
     )
-    for y in [17, 19]:
-        # Short rightwards arrow, from x=lower limit for HD 158259
-        ax.arrow(
-            inclinations_5pc["HD 158259"], y, 5, 0, head_width=0.8, head_length=1.4
-        )
 
     fg.supxlabel("$i$ [deg]", fontsize=16, y=0.02)
-    fg.supylabel("$\\Delta$", fontsize=18, x=0.01)
 
     fg.savefig(output_path, dpi=300, bbox_inches="tight")
 
